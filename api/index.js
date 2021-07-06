640f7a069d38873cefd90e2e28fc1ad02c715558
NishantTheProgrammer <knishant113@gmail.com>
2021-07-06 14:10:09 +0530

    workable on local network

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 
-const server = app.listen(3001, function() {
+const server = app.listen(3001, '192.168.1.243', function() {
     console.log('server running on port 3001');
 });
 
@@ -15,4 +15,8 @@ const io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
     console.log(socket.id)
+
+    socket.on('UPDATE_CORDS', function(data) {
+        io.emit('CORDS', data)
+    });
 });
